410cd9ab185553e530e637aed92e646daff4a6e8
Hirenbhajgotar <hirenbhajgotar.social@gmail.com>
2020-01-21 18:45:19 +0530

    ci_mail - verson-0.01

---
+++ application/controllers/User.php
@@ -0,0 +1,46 @@
+<?php 
+defined('BASEPATH') or exit('No direct script access allowed');
+class User extends CI_Controller
+{
+    public function index()
+    {
+        $this->form_validation->set_rules('email', 'email', 'required');
+        $this->form_validation->set_rules('password', 'password', 'required');
+        $this->form_validation->set_rules('password', 'password', 'required');
+        $this->form_validation->set_rules('confirm_password', 'confirm_password', 'required');
+        $this->form_validation->set_error_delimiters("<span class='text-danger text-small'><small>", "</small></span>");
+        if ($this->form_validation->run() === false) {
+            $this->load->view("user/register");
+        } else {
+            // echo 'run';
+
+            $this->load->library("email");
+            $config['protocol'] = 'smtp';
+            $config['mailpath'] = '/usr/sbin/sendmail';
+            // $config['charset'] = 'iso-8859-1';
+            $config['charset'] = 'utf-8';
+            $config['wordwrap'] = TRUE;
+
+            $this->email->initialize($config);
+            $this->email->from('bhajgotar8918514@gmail.com', 'Hiren');
+            $this->email->to($this->input->post('email'));
+
+            $this->email->subject('User registration');
+            $this->email->message('Testing the email class.');
+            $this->email->attach(base_url("assets/image/adult-book-business-297755.jpg"));
+
+            if ($this->email->send()) {
+                // redirect('/','refresh');
+               echo 'email has been sent';
+            } else {
+                show_error($this->email->print_debugger());
+            }
+
+            
+            
+        }
+
+    }
+}
+
+?>
